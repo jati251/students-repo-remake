69347e9133c2a26ad9c589a4d83776b59ba40706
Jati Suryo Buwono <jati.buwono@ist.id>
2024-09-08 19:54:34 +0700

    initial routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,21 @@
+// // src/routes/AppRoutes.tsx
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import HomePage from "../pages/HomePage";
+// import LoginPage from "../pages/LoginPage";
+// import DashboardLayout from "../pages/DashboardPage";
+// import NotFoundPage from "../pages/NotFoundPage";
+
+// const AppRoutes = () => (
+//   <Routes>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/login" element={<LoginPage />} />
+//     <Route path="/dashboard" element={<DashboardLayout />}>
+//       <Route index element={<DashboardHome />} />
+//       <Route path="settings" element={<DashboardSettings />} />
+//     </Route>
+//     <Route path="*" element={<NotFoundPage />} />
+//   </Routes>
+// );
+
+// export default AppRoutes;
---
+++ src/routes/PrivateRoutes.tsx
@@ -0,0 +1,15 @@
+// // src/routes/PrivateRoutes.tsx
+// import React from 'react';
+// import { Route, Navigate } from 'react-router-dom';
+
+// const PrivateRoute = ({ element, ...rest }) => {
+//   const isAuthenticated = // logic to check authentication
+//   return (
+//     <Route
+//       {...rest}
+//       element={isAuthenticated ? element : <Navigate to="/login" />}
+//     />
+//   );
+// };
+
+// export default PrivateRoute;
